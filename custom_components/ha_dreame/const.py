b72970036bafe0cd2936a68a91844aab89653b4e
Fredrik Hall <fredrik@hallsbyra.se>
2026-05-13 07:12:51 +0200

    feat: add robot command gate option

--- custom_components/ha_dreame/const.py
+++ custom_components/ha_dreame/const.py
@@ -1,5 +1,6 @@
 """Constants for HA Dreame."""
 
+CONF_ALLOW_ROBOT_COMMANDS = "allow_robot_commands"
 CONF_VACUUM_ENTITY_ID = "vacuum_entity_id"
 DREAME_VACUUM_DOMAIN = "dreame_vacuum"
 DOMAIN = "ha_dreame"
